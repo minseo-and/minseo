75642696fd266749e6a32b7565c1a22103a3c413
Minseo <msp05070478@gmail.com>
2021-04-02 15:46:26 +0900

    [ADD]Undefinded

--- project.c
+++ project.c
@@ -2,28 +2,25 @@
  int main()
  {
  	int a, i;
- 	int arr[9];
-	scanf("%d", &a);
- 	switch(a)
- 	{
- 
-		 
+ 	int arr[10];
+ 	while(1){
+		scanf("%d", &a);
 		
- 		case 1:
- 			for(i=0; i<10; i++)
- 			scanf("%d", &arr[i]);
- 			break;
- 		case 2:
- 			for(i=0; i<10; i++)
- 			printf("%d", arr[i]);
- 			break;
- 		case 3:
- 			printf("언디파인드 파이팅!");
- 			break;
- 			default:
- 				break;
- 			
-}
-
-	 return 0;
+	 	switch(a) {
+	 		case 1:
+	 			for(i=0; i<10; i++)
+	 				scanf("%d", &arr[i]);
+	 			break;
+	 		case 2:
+	 			for(i=0; i<10; i++)
+	 				printf("%d ", arr[i]);
+	 			break;
+	 		case 3: 
+			 	printf("언디파이드 파이팅!"); 
+			 	return 0;
+	 		default:
+	 			break;
+ 	}
+ }
+	return 0;
  }
